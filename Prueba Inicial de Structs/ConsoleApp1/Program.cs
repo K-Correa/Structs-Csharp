047e667cbb4ca5b3b1237f36d92a4fcf6eb77cde
KCorrea091 <KCorrea091@outlook.com>
2020-11-22 11:36:49 -0400

    first commit

---
+++ Prueba Inicial de Structs/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        struct Nacimiento
+        {
+            public int Mes;
+            public int Dia;
+            public int Año;
+
+            //constructor
+
+            public Nacimiento(int dia, int mes, int año)
+            {
+                this.Dia = dia;
+                this.Mes = mes;
+                this.Año = año;
+            }
+            //Metodos
+            public void AsignarNacimiento(int dia, int mes, int año)
+            {
+                this.Dia = dia;
+                this.Mes = mes;
+                this.Año = año;
+            }
+
+            public override string ToString()
+            {
+                return Dia + "/" + Mes + "/" + Año;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Nacimiento fecha;
+            Nacimiento fecha2 = new Nacimiento();
+            Nacimiento fecha3 = new Nacimiento(12, 1, 1992);
+
+            fecha.Dia = 12;
+            fecha.Mes = 4;
+            fecha.Año = 1900;
+
+            Console.WriteLine("El dia de nacimiento es {0}", fecha.Dia);
+            fecha.AsignarNacimiento(1, 1, 2020);
+            Console.WriteLine("La nueva fecha es {0}", fecha);
+        }
+    }
+}
---
+++ Prueba Inicial de Structs/Ejercicio1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    /* Crea un "struct" que almacene datos de una canción en formato MP3: Artista, Título, Duración (en segundos),
+Tamaño del fichero (en KB). Un programa debe pedir los datos de una canción al usuario, almacenarlos en
+dicho "struct" y después mostrarlos en pantalla. */
+    class Program
+    {
+        struct Canciones
+        {
+            public string Artista;
+            public string Titulo;
+            public double Duracion;
+
+            public Canciones(string artista, string titulo, double duracion)
+            {
+                this.Artista = artista;
+                this.Titulo = titulo;
+                this.Duracion = duracion;
+            }
+            public void AsignarDatosCancion(string artista, string titulo, double duracion)
+            {
+                this.Artista = artista;
+                this.Titulo = titulo;
+                this.Duracion = duracion;
+            }
+            public override string ToString()
+            {
+                return $"\nCancion: {Titulo} \n Artista: {Artista} \n Duracion: {Duracion}";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Cual es el artista?: ");
+            string setArtista = Convert.ToString(Console.ReadLine());
+            Console.Write("Indique el titulo del artista: ");
+            string setTitulo = Convert.ToString(Console.ReadLine());
+
+            Console.Write("De cuanto es la duracion? ");
+
+            double setDuracion = Convert.ToDouble(Console.ReadLine());
+
+            Canciones canciones = new Canciones();
+
+            canciones.AsignarDatosCancion(setArtista, setTitulo, setDuracion);
+            Console.WriteLine(canciones);
+        }
+    }
+}
